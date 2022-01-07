6fe3f973132d061c650e4f65d306295d91e1916c
Sonia Zhang <wenyuez@usc.com>
2022-01-06 23:58:56 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true}, 
+    ()=>{
+        console.log("Connected to MongoDB!");
+    }
+);
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Home Page')
+  })
+
+app.get('/users', (req, res) => {
+    res.send('Welcome to user Page')
+})
+
+app.listen(8800, ()=>
+    {
+        console.log("Backend server is running!");
+    }
+);
