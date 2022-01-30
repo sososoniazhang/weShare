a695fa174f3fde9f36be360509fe1059b231db58
Sonia Zhang <wenyuez@usc.com>
2022-01-30 08:57:28 -0800

    register

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,6 +1,23 @@
 import "./register.css";
+import { useRef } from "react";
 
 export default function Register() {
+  const username = useRef();
+  const email = useRef();
+  const passwordAgain = useRef();
+  const password = useRef();
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    if (password.current.value !== passwordAgain.current.value) {
+      password.current.setCustomValidity("Passwords do not match!")
+      console.log("not match")
+    } else { 
+      const user = {
+        
+      }
+    }
+  }
   return (
     <div className="login">
       <div className="loginWrapper">
@@ -11,16 +28,38 @@ export default function Register() {
           </span>
         </div>
         <div className="loginRight">
-          <div className="loginBox">
-            <input placeholder="Username" className="loginInput" />
-            <input placeholder="Email" className="loginInput" />
-            <input placeholder="Password" className="loginInput" />
-            <input placeholder="Password Again" className="loginInput" />
+          <form className="loginBox" onSubmit={handleClick}>
+            <input 
+              placeholder="Username" 
+              className="loginInput" 
+              ref={username} 
+              required/>
+            <input 
+              placeholder="Email" 
+              type="email"
+              className="loginInput" 
+              ref={email} 
+              required/>
+            <input 
+              placeholder="Password" 
+              type = "password"
+              className="loginInput" 
+              ref={password} 
+              required 
+              minLength={6}/>
+            
+            <input 
+              placeholder="Password Again" 
+              className="loginInput" 
+              type = "password"
+              ref={passwordAgain} 
+              required 
+              minLength={6}/>
             <button className="loginButton">Sign Up</button>
             <button className="loginRegisterButton">
               Log into Account
             </button>
-          </div>
+          </form>
         </div>
       </div>
     </div>
